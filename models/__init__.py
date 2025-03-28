adf098a9638b09a9032e4f1eab09e1ebe7e8266e
pablovanegas <104100454+pablovanegas@users.noreply.github.com>
2025-03-28 18:16:37 -0500

    class version
    to be consumed by an API

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,13 @@
-from utils.import_data import load_data
-from utils.preprocessing import feature_engineering, scale_data
-from models.lstm_model import build_lstm_model
+from models.rf_model import train_random_forest
+from models.lstm_model import train_lstm_model
+from models.sequential_model import train_sequential_model
+from models.xgb_model import train_xgb_model
 from models.nbeats_model import create_multivariate_nbeats
-from utils.evaluation import evaluate_regression
-from utils.visualizations import plot_predictions
-import pandas as pd
+
 __all__ = [
-    "load_data",
-    "feature_engineering",
-    "scale_data",
-    "build_lstm_model",
+    "train_random_forest",
+    "train_lstm_model",
+    "train_sequential_model",
+    "train_xgb_model",
     "create_multivariate_nbeats",
-    "evaluate_regression",
-    "plot_predictions",
 ]
